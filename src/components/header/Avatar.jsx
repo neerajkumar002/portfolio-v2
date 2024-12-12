8bbbe9ff64200539f87bcfc7825806db002a592d
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-12 17:22:58 +0530

    added avatar

---
+++ src/components/header/Avatar.jsx
@@ -0,0 +1,13 @@
+const Avatar = () => {
+  return (
+    <div className="rounded-full w-[100px] h-[100px]">
+      <img
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+        alt="NK"
+        className="  rounded-full"
+      />
+    </div>
+  );
+};
+
+export default Avatar;

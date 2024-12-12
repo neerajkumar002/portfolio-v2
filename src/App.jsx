77351c2531d5d2dcd3fbe19d51d854aaab80a2be
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-12-12 17:21:37 +0530

    Added some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes></Routes>
+      <Layout >
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
       </Layout>
     </BrowserRouter>
   );
